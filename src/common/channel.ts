ea550829463819282904fcc746bce4ac77ad5893
Luca <luca@ahd-creative.agency>
2024-03-26 17:44:40 +0100

    feat(common): installer settings

---
+++ src/common/channel.ts
@@ -0,0 +1,27 @@
+export default {
+    window: {
+        minimize: 'window/minimize',
+        maximize: 'windows/maximize',
+        close: 'window/close',
+        isMaximized: 'window/isMaximized'
+    },
+    update: {
+        error: 'update/error',
+        available: 'update/available',
+        downloaded: 'update/downloaded'
+    },
+    checkForInstallerUpdate: 'checkForInstallerUpdate',
+    installManager: {
+        fragmenterEvent: 'installManager/fragmenterEvent',
+        installFromUrl: 'installManager/installFromUrl',
+        cancelInstall: 'installManager/cancelInstall',
+        uninstall: 'installManager/uninstall'
+    },
+    sentry: {
+        requestSessionID: 'sentry/requestSessionID',
+        provideSessionID: 'sentry/provideSessionID'
+    },
+    openPath: 'openPath',
+    msfsBasePathSelectionDialog: 'msfsBasePathSelectionDialog',
+    xplaneBasePathSelectionDialog: 'xplaneBasePathSelectionDialog'
+}
